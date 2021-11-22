3b21ace1edcd65dc81273b7e4f20a5ebe33adc82
Ornella Ourfi <ornella93@outlook.fr>
2021-11-22 18:01:46 +0100

    ajout du snipper + bloquer la partie de supprssion de cv quand le snipper est activer

--- src/components/connected/MonEspace.js
+++ src/components/connected/MonEspace.js
@@ -19,6 +19,7 @@ function MonEspace() {
   const isUploaded = useSelector(state => state.conseiller?.isUploaded);
   const candidat = useSelector(state => state.conseiller?.conseiller);
   const isDeleted = useSelector(state => state.conseiller?.isDeleted);
+  const loadingDeleteCv = useSelector(state => state.conseiller?.loadingDeleteCv);
   const uploading = useSelector(state => state.conseiller?.uploading);
   const updateError = useSelector(state => state?.user?.patchError);
   const user = useSelector(state => state.authentication.user.user);
@@ -76,6 +77,12 @@ function MonEspace() {
 
   return (
     <div className="informations">
+      <Spinner
+        style={{ textAlign: 'center' }}
+        type="Oval"
+        color="#00BFFF"
+        visible={loadingDeleteCv === true}
+      />
       { isUploaded &&
           <div className="fr-col-12 fr-mb-3w">
             <FlashMessage duration={100000} >
@@ -174,7 +181,8 @@ function MonEspace() {
               <p>Vous n&apos;avez pas encore téléchargé votre Curriculum vit&aelig;, faites le dès maintenant ! </p>
             }
             <span>Ajouter ou mettre à jour votre CV&nbsp;:</span>
-            <div className={fileRejections.length > 0 ? 'dropZone drop-error fr-mt-4w fr-mb-5w' : 'fr-btn fr-mt-4w fr-mb-5w upload-btn' }
+            <div style={{ display: 'flex' }}
+              className={fileRejections.length > 0 ? 'dropZone drop-error fr-mt-4w fr-mb-5w' : 'fr-btn fr-mt-4w fr-mb-5w upload-btn' }
               {...getRootProps()}>
               <input {...getInputProps()} />
               { acceptedFiles.length === 0 &&
@@ -208,9 +216,14 @@ function MonEspace() {
                 <span className="fr-fi-file-download-line download-img" aria-hidden="true"></span>
                 Mon CV ({candidat?.cv?.file})
               </button>
-              <p className="supprimer-link fr-mt-2w" onClick={deleteCV}>
+              { loadingDeleteCv === true ?
+                <p className="supprimer-link fr-mt-2w" style={{ color: '#eeeeee' }}>
                   Supprimer votre CV
-              </p>
+                </p> :
+                <p className="supprimer-link fr-mt-2w" onClick={deleteCV}>
+                  Supprimer votre CV
+                </p>
+              }
             </>
             }
           </div>

bf8195432956bd957a60a44447bd138f0cd675ad
Ornella <68587983+Ornella452@users.noreply.github.com>
2022-10-10 12:12:35 +0200

    Update src/components/connected/MonEspace.js
    Co-authored-by: Sébastien Quirrion <77339872+squirrion@users.noreply.github.com>

--- src/components/connected/MonEspace.js
+++ src/components/connected/MonEspace.js
@@ -265,7 +265,7 @@ function MonEspace() {
             <br />(lien personnel à ne pas partager)
             <h2 className="fr-mt-8w" style={{ marginLeft: '-2px' }}>Comment consulter mes r&eacute;sultats PIX ?</h2>
             <p>
-              Pour consulter vos r&eacute;sultats, il vous suffit de vous connecter à votre compte personnel Pix,
+              Pour consulter vos r&eacute;sultats, il vous suffit de vous connecter &agrave; votre compte personnel Pix,
               vous pourrez alors retrouver vos r&eacute;sultats depuis la page <strong>&quot;Mes certifications&quot;</strong>.
               Elle est accessible depuis la rubrique <strong>&quot;Certification&quot;</strong>
               en cliquant sur <strong>&quot;Voir mes certifications&quot;</strong>.
